03d766edb5f421b20c55bc1fdf97fae204989d6a
vincent <vincentheymans@gmail.com>
2013-05-08 11:27:59 +0200

    fizzbuzz finaal added

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -3,24 +3,31 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title>The FizzyBuzz</title>
+
+<link rel="stylesheet" href="style.css" media="screen">
+<link rel="stylesheet" href="normalize.css" media="all">
+
+
 </head>
 
 <body>
+
 <?php
 
 	for($i = 1; $i <=100; $i++ )
-	{
-		if($i % 3 == 0)
+	{if($i % 3 == 0 && $i % 5 == 0){
+			echo "<div id='fizzbuzz'>fizzbuzz</div>";}
+		else if($i % 3 == 0)
 		{
-			echo "<div class='fizz'>Fizz</div>";
+			echo "<div id='fizz'>Fizz</div>";
 		}
+		
 		else if ($i % 5 == 0)
 		{
-			echo "<div class='buzz'>Buzz</div>";
+			echo "<div id='buzz'>Buzz</div>";
 		}
-		if($i % 3 == 0 && $i % 5 == 0){
-			echo "<div class='fizzbuzz'>fizzbuzz</div>";}
+		
 		else
 		{
 			echo $i . " </br>";
